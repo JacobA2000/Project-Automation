11a51c459c7bd6e267ab5d3bb3cd6aada0b7e34e
JacobA2000 <jacoballen785@gmail.com>
2020-11-08 16:13:40 +0000

    Added setup script and terminal script creation

--- AutomateProject.py
+++ AutomateProject.py
@@ -2,6 +2,7 @@ import requests
 import json
 import subprocess
 import os
+import sys
 
 #Terminal Colours
 class Color():
@@ -10,27 +11,15 @@ class Color():
     CYAN = '\033[96m'
     ENDC = '\033[0m'
 
+#Path this file is stored at.
+myPath = os.path.dirname(os.path.abspath(__file__))
+
 projectsDir = ""
 gitUsername = ""
 gitToken = ""
 
-#Checks if a config file exists
-if os.path.exists("config.json") == False:
-    #If theres no config file create one
-    print("First Time Setup")
-    #Get user data to populate config file.
-    projectsPath = input("Projects Path (the path you wish your projects to be cloned to) : ")
-    uname = input("GitHub username : ")
-    token = input("GitHub Token (your GitHub personal access token, you can get one here https://github.com/settings/tokens) : ")
-
-    userConfig = {"projectsDir": projectsPath, "gitUsername": uname, "gitToken": token}
-
-    #Create and add data to config file.
-    with open("config.json", "w") as f:
-        json.dump(userConfig, f)
-
 #Get config data from config.json file
-with open("config.json", "r") as f:
+with open(f"{myPath}\\config.json", "r") as f:
     configData = json.load(f)
     projectsDir = configData["projectsDir"]
     gitUsername = configData["gitUsername"]
---
+++ Setup.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+#Checks if a config file exists
+if os.path.exists("config.json") == False:
+    #If theres no config file create one
+    print("First Time Setup")
+    #Get user data to populate config file.
+    projectsPath = input("Projects Path (the path you wish your projects to be cloned to) : ")
+
+    if projectsPath.endswith("\\") == False:
+        projectsPath += "\\"
+
+    uname = input("GitHub username : ")
+    token = input("GitHub Token (your GitHub personal access token, you can get one here https://github.com/settings/tokens) : ")
+
+    userConfig = {"projectsDir": projectsPath, "gitUsername": uname, "gitToken": token}
+
+    #Create and add data to config file.
+    with open("config.json", "w") as f:
+        json.dump(userConfig, f)
+
+#Create relevant terminal script based on OS.
+if os.name == "nt":
+    #Checks if .bat file exists
+    if os.path.exists("createproj.bat") == False:
+        #Create bat file to run quickly on windows.
+        with open("createproj.bat", "w") as batf:
+            batf.write(f"py \"{os.getcwd()}\\AutomateProject.py\"")
+            
+else:
+    if os.path.exists("createproj.sh") == False:
+        #Create bat file to run quickly on windows.
+        with open("createproj.sh", "w") as batf:
+            batf.write(f"python3 \"{os.getcwd()}\\AutomateProject.py\"")
